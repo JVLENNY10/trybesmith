de4474d8df347dfb5c34abe93b260b269b05ed15
João Victor <joao.lenny100@gmail.com>
2022-05-13 19:21:58 -0300

    checkName
    - Criando função checkName no arquivo products.middlewares.ts

--- src/middlewares/products.middlewares.ts
+++ src/middlewares/products.middlewares.ts
@@ -1,1 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+// import ProductsServices from '../services/products.services';
 
+export const checkName = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body;
+
+  if (name === undefined) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+
+  if (typeof name !== 'string') {
+    return res.status(422).json({ message: '"name" must be a string' });
+  }
+
+  if (name.length < 3) {
+    return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
+  }
+
+  next();
+};
+
+export default checkName;
