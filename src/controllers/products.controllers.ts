73f475539206165c34a3febe34d19a9bf8a32037
João Victor <joao.lenny100@gmail.com>
2022-05-13 19:07:09 -0300

    Controller create
    - Criando controller create no arquivo products.controllers.ts

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import ProductsServices from '../services/products.services';
 
-const getAll = async (_req: Request, res: Response): Promise<Response> => {
+export const getAll = async (_req: Request, res: Response): Promise<Response> => {
   const services = new ProductsServices();
   const products = await services.getAll();
   return res.status(200).json(products);
 };
 
-export default getAll;
+export const create = async (req: Request, res: Response): Promise<Response> => {
+  const services = new ProductsServices();
+  const newProduct = await services.create(req.body);
+  return res.status(201).json(newProduct);
+};
