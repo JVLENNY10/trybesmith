b1b55cb12ad97c9a539ad47fb31aed8eb4299c9a
João Victor <joao.lenny100@gmail.com>
2022-05-13 20:21:25 -0300

    Adicionando usersRoutes em app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import usersRoutes from './routes/users.routes';
 import productsRoutes from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(productsRoutes);
+app.use(usersRoutes);
 
 export default app;
