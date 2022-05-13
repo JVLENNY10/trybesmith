7a38926480a6534118ff635dbee681111e49449d
João Victor <joao.lenny100@gmail.com>
2022-05-13 20:04:56 -0300

    checkPassword
    - Criando middleware checkPassword

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -55,3 +55,23 @@ export const checkLevel = (req: Request, res: Response, next: NextFunction) => {
 
   next();
 };
+
+export const checkPassword = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+
+  if (password === undefined) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(422).json({ message: '"password" must be a string' });
+  }
+
+  if (password.length < 8) {
+    return res.status(422).json({
+      message: '"password" length must be at least 8 characters long',
+    });
+  }
+
+  next();
+};
