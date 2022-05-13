8aa5176d62c69cb5de8e61e9ce8fdbaaaf23f7fe
João Victor <joao.lenny100@gmail.com>
2022-05-13 19:04:26 -0300

    Service create
    - Criando service create no arquivo products.services.ts

--- src/services/products.services.ts
+++ src/services/products.services.ts
@@ -12,6 +12,11 @@ class ProductsServices {
     const products = await this.models.getAll();
     return products;
   };
+
+  public create = async (product: IProducts): Promise<IProducts> => {
+    const newProduct = await this.models.create(product);
+    return newProduct;
+  };
 }
 
 export default ProductsServices;
