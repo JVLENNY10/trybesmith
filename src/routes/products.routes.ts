cced3b1e9510309ba1547535a58ee79a64882c34
João Victor <joao.lenny100@gmail.com>
2022-05-13 19:32:36 -0300

    Requisito 2
    - Adicionando requisição do tipo POST no arquivo products.routes.ts;
    - Usando as middlewares checkName e checkAmount na requisição;
    - Usando a controller create na requisição

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import getAll from '../controllers/products.controllers';
+import { getAll, create } from '../controllers/products.controllers';
+import { checkName, checkAmount } from '../middlewares/products.middlewares';
 
 const routes = Router();
 
 routes.get('/products', getAll);
+routes.post('/products', checkName, checkAmount, create);
 
 export default routes;
