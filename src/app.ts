cc2f3434dacf0132b0b3040f8058771d2c7e459e
João Victor <joao.lenny100@gmail.com>
2022-05-13 21:30:59 -0300

    app.use(ordersRoutes)
    - Adicionando ordersRoutes no arquivo app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import usersRoutes from './routes/users.routes';
+import ordersRoutes from './routes/orders.routes';
 import productsRoutes from './routes/products.routes';
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(productsRoutes);
 app.use(usersRoutes);
+app.use(ordersRoutes);
 
 export default app;
