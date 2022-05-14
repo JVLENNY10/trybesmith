7838fa1e0f2e6ed6a9ceff0e236e3d3c8a167f46
João Victor <joao.lenny100@gmail.com>
2022-05-13 21:14:07 -0300

    getAll
    - Criando arquivo orders.models.ts;
    - Adicionando informações ao arquivo orders.modeles.ts: Criando model getAll

---
+++ src/models/orders.models.ts
@@ -0,0 +1,19 @@
+import { Pool } from 'mysql2/promise';
+
+import connection from './connection';
+import IOrders from '../interfaces/orders.interface';
+
+class OrdersModels {
+  private connection: Pool;
+
+  constructor() {
+    this.connection = connection;
+  }
+
+  public getAll = async (): Promise<IOrders[]> => {
+    const [orders] = await this.connection.execute('SELECT * FROM Trybesmith.Orders');
+    return orders as IOrders[];
+  };
+}
+
+export default OrdersModels;
