42d2a9d0f094a5ae3a783d69851ce0bafb2ff2f1
João Victor <joao.lenny100@gmail.com>
2022-05-22 15:05:12 -0300

    Nova service
    Criando service getAll no arquivo orders.services.ts

--- src/services/orders.services.ts
+++ src/services/orders.services.ts
@@ -1,16 +1,30 @@
 import OrdersModels from '../models/orders.models';
+import ProductsModels from '../models/products.models';
 import IOrders from '../interfaces/orders.interface';
 
 class OrdersServices {
-  private models: OrdersModels;
+  private ordersModels: OrdersModels;
+
+  private productsModels: ProductsModels;
 
   constructor() {
-    this.models = new OrdersModels();
+    this.ordersModels = new OrdersModels();
+    this.productsModels = new ProductsModels();
   }
 
   public getAll = async (): Promise<IOrders[]> => {
-    const orders = await this.models.getAll();
-    return orders;
+    const ordersModels = await this.ordersModels.getAll();
+    const productsModels = await this.productsModels.getAll();
+
+    const result = ordersModels.map((order) => ({
+      id: order.id,
+      userId: order.userId,
+      productsIds: productsModels
+        .filter((product) => product.orderId === order.id)
+        .map((product) => product.id),
+    }));
+
+    return result;
   };
 }
 
