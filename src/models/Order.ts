682a7951b0fff842108fce6f627bc3db4745ae26
João Victor <joao.lenny100@gmail.com>
2022-10-11 17:43:34 -0300

    Model - Order.ts
    - Criando método "create".

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 
 import connection from './connection';
 import IOrders from '../interfaces/ordersInterface';
@@ -9,6 +9,14 @@ class Order {
   constructor() {
     this.connection = connection;
   }
+
+  public create = async (userId: number): Promise<number> => {
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(`
+      INSERT INTO Trybesmith.Orders (userId) VALUES (?);
+    `, [userId]);
+
+    return insertId;
+  };
 
   public getAll = async (): Promise<IOrders[]> => {
     const [orders] = await this.connection.execute('SELECT * FROM Trybesmith.Orders;');
