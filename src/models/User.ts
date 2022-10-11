867098f50a474e0b85e0c1fbca314cef9ec0e970
João Victor <joao.lenny100@gmail.com>
2022-10-11 13:07:56 -0300

    Models - User.ts
    - Editando o método create;
    - Criando o método login.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
 
 import connection from './connection';
-import IUsers from '../interfaces/usersInterface';
+import { ILogin, IUsers } from '../interfaces/usersInterface';
 
 class User {
   private connection: Pool;
@@ -10,14 +10,24 @@ class User {
     this.connection = connection;
   }
 
-  public create = async (infos: IUsers): Promise<number> => {
+  public create = async (infos: IUsers): Promise<IUsers> => {
     const { username, classe, level, password } = infos;
 
-    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(`
+    const [{ insertId: id }] = await this.connection.execute<ResultSetHeader>(`
       INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);
     `, [username, classe, level, password]);
 
-    return insertId;
+    return { id, ...infos };
+  };
+
+  public login = async (infos: ILogin): Promise<ILogin[]> => {
+    const { username, password } = infos;
+
+    const [userLogin] = await connection.execute(`
+      SELECT id, username, password FROM Trybesmith.Users WHERE username = ? and password = ?;
+    `, [username, password]);
+
+    return userLogin as ILogin[];
   };
 }
 
