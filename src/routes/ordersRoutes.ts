9ba72fa3494ce17e765d894481af987d0297c1a1
João Victor <joao.lenny100@gmail.com>
2022-10-11 17:47:29 -0300

    Routes - ordersRoutes.ts
    - Criando rota do tipo POST para "/orders".

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+import UsersMiddlewares from '../middlewares/UsersMiddlewares';
 import OrdersController from '../controllers/OrdersController';
+import OrdersMiddlewares from '../middlewares/OrdersMiddlewares';
 
 const routes = Router();
+const usersMiddlewares = new UsersMiddlewares();
 const ordersController = new OrdersController();
+const ordersMiddlewares = new OrdersMiddlewares();
 
-const { getAll } = ordersController;
+const { checkToken } = usersMiddlewares;
+const { create, getAll } = ordersController;
+const { checkProductsIds } = ordersMiddlewares;
 
 routes.get('/orders', getAll);
+routes.post('/orders', checkToken, checkProductsIds, create);
 
 export default routes;
